b861b682adfc07ddecc3e85ee49608330a2a53a7
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-09-06 13:09:04 -0400

    db connection

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// middleware
+app.use(express.static('public'));
+
+// database connection
+const dbURI = 'mongodb+srv://mps5199:test1234@cluster0.del96.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
