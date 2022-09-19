ca398b53accf1feb27b7323c972e873abe4e5a97
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-09-19 13:09:04 -0400

    using auth routes in app.js

--- app.js
+++ app.js
@@ -19,3 +19,4 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
