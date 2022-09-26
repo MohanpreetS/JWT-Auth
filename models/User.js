e6333c67375d19536d60e3b75df4eff1c8597238
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-09-26 13:09:04 -0400

    pre middleware

--- models/User.js
+++ models/User.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter an email'],
     unique: true,
     lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
   }
+});
+
+
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+  console.log('user about to be created & saved', this);
+  next();
 });
 
 const User = mongoose.model('user', userSchema);
