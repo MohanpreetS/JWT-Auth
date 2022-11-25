7c025590c14f5241b519a2505b31c7d70079b5c4
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-11-25 13:09:04 -0500

    set up google login route

--- oauth/routes/auth-routes.js
+++ oauth/routes/auth-routes.js
@@ -11,4 +11,10 @@ router.get('/logout', (req, res) => {
     res.send('logging out');
 });
 
+// auth with google+
+router.get('/google', (req, res) => {
+    // handle with passport
+    res.send('logging in with Google');
+});
+
 module.exports = router;
