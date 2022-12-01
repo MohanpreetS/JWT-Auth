04593a2cdc9823b50ad9d332d136ce6aef923e44
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-12-01 13:09:04 -0500

    adding keys and redacting them from the public github repo

---
+++ oauth/config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: 'redacted client id',
+            clientSecret: 'redacted client secret'
+        }
+};
