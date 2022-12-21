7f828912e90c1798c0d6cd887dedf7ee722906db
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-12-21 13:09:04 -0500

    deserialize user

--- oauth/config/passport-setup.js
+++ oauth/config/passport-setup.js
@@ -7,6 +7,12 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
         // options for google strategy
